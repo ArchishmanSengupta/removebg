46305d8b5c6d42d0af198ed417b959aadc1cc20d
Archishman Sengupta <archishman1808@gmail.com>
2024-12-09 01:10:38 +0530

    feat: convert imageprovider to Uint8List

--- lib/removebg.dart
+++ lib/removebg.dart
@@ -1,6 +1,10 @@
 library removebg;
 
+import 'dart:async';
+import 'dart:ui';
+
 import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
 
 /// A background removal and text rerenderer for images.
 class Removebg {
@@ -11,10 +15,42 @@ class Removebg {
   static Future<ImageProvider> removebg(ImageProvider imageProvider_) async {
     try {
       // Convert ImageProvider to Unint8List
+
+      Uint8List? imageBytes = await _imageProviderToBytes(imageProvider_);
       return imageProvider_;
     } catch (e) {
       print('Background removal error: $e');
       return imageProvider_;
+    }
+  }
+
+  /// Converts an [ImageProvider] to a [Uint8List] in PNG format.
+  ///
+  /// - Parameter [imageProvider]: The [ImageProvider] to be converted.
+  /// - Returns: A [Future] that completes with the [Uint8List] representation
+  ///   of the image.
+  static Future<Uint8List?> _imageProviderToBytes(
+      ImageProvider imageProvider) async {
+    try {
+      final completer = Completer<Uint8List>();
+
+      imageProvider.resolve(const ImageConfiguration()).addListener(
+            ImageStreamListener(
+              (ImageInfo imageInfo, bool synchronousCall) async {
+                final ByteData? byteData = await imageInfo.image.toByteData(
+                  format: ImageByteFormat.png,
+                );
+                completer.complete(byteData?.buffer.asUint8List());
+              },
+              onError: (dynamic exception, StackTrace? stackTrace) {
+                completer.completeError(exception, stackTrace);
+              },
+            ),
+          );
+      return completer.future;
+    } catch (e) {
+      pragma('Image conversion error: $e');
+      return null;
     }
   }
 }
